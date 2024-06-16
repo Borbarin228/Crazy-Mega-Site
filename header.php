10ea9f47642525c368cf0c4ea6f6e78adb406c57
Aboldosus <114693651+Borbarin228@users.noreply.github.com>
2024-06-17 00:18:32 +0500

    на сегодня все

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header class = "head">
+
+<div class = "container">
+    <div class = "header_txt">
+        <a href="">ИНФОРМАЦИЯ</a>
+        <a href="">ТВОРЧЕСТВО</a>
+        <a href="">КУПИТЬ БИЛЕТЫ</a>
+
+    </div>
+
+    <div class = "header_img">
+        <button class="regButton"></button>
+    </div>
+</div>
+
+</header>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crazy Site</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="footer.css">
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="form.css">
+</head>
+<body>
+
+
+<!--подключение хэдэра-->
+    <?php require "header.php"?>
+    
+<!--индекс-->
+    <main>
+
+<!--лицевая часть-->
+<div>
+<div class="face">
+    <img src="assets/main1.png" alt="" id = "main1">
+    <img src="assets/main2.png" alt="" id = "main2">
+    <img src="assets/main3.png" alt="" id = "main3">
+    
+</div>
+    <img src="assets/background_part.png" alt="" id="bPart">
+</div>
+<!--творчество-->
+<div class = "art">
+
+<div class = "fstAlbomes">
+<img src="assets/payday.png" alt="">
+<img src="assets/moodblood.png" alt="">
+</div>
+
+<div class = "scndAlbomes">
+<img src="assets/antiwrld.png" alt="">
+<img src="assets/rawww.png" alt="">
+</div>
+
+</div>
+
+<!--список концертов-->
+<div>
+
+</div>
+
+<!--мерч-->
+<div>
+
+</div>
+
+
+    </main>
+
+<!--подключение футера-->
+    <?php require "footer.php"?>
+</body>
+</html>
