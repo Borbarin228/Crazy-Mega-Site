3cb0f096086c23ac522016af9cc21f7570cffc1a
Aboldosus <114693651+Borbarin228@users.noreply.github.com>
2024-06-20 15:12:42 +0500

    МАСШТАБНОЕ ОБНОВЛЕНИЕ 2.0
    заходите мы не украдем данные с вашей карты потому что мы уже это сделали

--- form.php
+++ form.php
@@ -1,3 +1,11 @@
+<?
+session_start();
+setcookie("project_name", $_POST['project_name'], time()+1000,"/");
+setcookie("description", $_POST['description'], time()+1000,"/");
+setcookie("average_rating", $_POST['average_rating'], time()+1000,"/");
+setcookie("url_read", $_POST['url_read'], time()+1000,"/");
+?>
+
 <div class = "modal ">
 
 <div class = "form">
@@ -7,8 +15,8 @@
       
         <form method ="POST" action="auth.php" class = "content">
         <p><b>ВХОД</b></p>
-          <input class="box" type="text" name="login" id="login" placeholder="Логин" require>
-          <input class="box" type="password" name="password" id="password" placeholder="Пароль" require>
+          <input class="box" type="text" name="login" id="login" placeholder="Логин" require value = "<?if(isset($_COOKIE['login']) && '' !== $_COOKIE['login']) echo $_COOKIE['login']?>">
+          <input class="box" type="password" name="password" id="password" placeholder="Пароль" require value = "<?if(isset($_COOKIE['password']) && '' !== $_COOKIE['password']) echo $_COOKIE['pasword']?>">
 
           <button class = "enteryBtn">ВОЙТИ</button>
           <a href="register.php">РЕГИСТРАЦИЯ</a>
